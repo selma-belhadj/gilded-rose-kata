548a01ccb6205d0fa3f0bb700ed87e1a1bed3020
selma <belhadjselma1@gmail.com>
2022-12-08 23:16:40 +0100

    Add unit test for Item function

---
+++ js/test/gilded_rose.test.js
@@ -0,0 +1,34 @@
+import update_quality from '../src/gilded_rose';
+import Item from '../src/gilded_rose';
+
+describe("Item", function() {
+  let items = [];
+  it("should foo", function() {
+    items = [ new Item("foo", 0, 0) ];
+    expect(items[0].name).toEqual("foo");
+  });
+
+  it("should foo", function() {
+    items = [ new Item("foo", 0, 0) ];
+    expect(items[0].sell_in).toEqual(0);
+  });
+
+  it("should foo", function() {
+    items = [ new Item("foo", 0, 0) ];
+    expect(items[0].quality).toEqual(0);
+  });
+
+});
+
+describe("update_quality", function() {
+  let Item1= new Item('foo1', 0, 0);
+  let Item2= new Item('foo2', 0, 0);
+  let Item3= new Item('foo3', 0, 0);
+  let items = [Item1, Item2, Item3]; 
+
+  it("should foo", function() {
+    // items = [ new Item("foo", 0, 0) ];
+    update_quality();
+    expect(items[0].name).toEqual("fixme");
+  });
+});
