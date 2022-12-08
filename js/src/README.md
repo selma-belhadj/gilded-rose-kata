3c2f929acc1529d4abf0101dc30534ead5d7dc41
selma <belhadjselma1@gmail.com>
2022-12-08 23:30:43 +0100

    Define modifyQuality & modifySellIn functions

--- js/src/README.md
+++ js/src/README.md
@@ -1,2 +1,9 @@
 1. Use let instead of var
     > let allows you to declare variables that are limited to the scope of a block statement, or expression on which it is used, unlike the var keyword, which declares a variable globally, or locally to an entire function regardless of block scope
+2. Variable and function names are written in camelCase
+   1. Choose a word with meaning (provide some context)
+   2. Attach additional information to a name (use suffix or prefix)
+3. Don’t repeat yourself (DRY)
+4. you aren’t gonna need it (YAGNI)
+5. keep it simple, stupid principle (KISS)
+   
--- js/src/gilded_rose_improved.js
+++ js/src/gilded_rose_improved.js
@@ -8,6 +8,20 @@ function Item(name, sellIn, quality) {
   this.quality = quality;
 }
 
+function modifyQuality(degree) {
+  if (degree ==  0) {
+    return this.quality -= this.quality
+  }
+  return this.quality += this.quality
+}
+
+function modifySellIn(degree) {
+  if (degree ==  0) {
+    return this.sellIn -= this.sellIn
+  }
+  return this.sellIn += degree
+}
+
 let items = []
 
 export function updateQuality() {
@@ -15,21 +29,21 @@ export function updateQuality() {
     if (items[i].name != 'Aged Brie' && items[i].name != BACKSTAGE) {
       if (items[i].quality > 0) {
         if (items[i].name != SULFURAS) {
-          items[i].quality = items[i].quality - 1
+          items[i].quality = modifyQuality(-1)
         }
       }
     } else {
       if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
+        items[i].quality = modifyQuality(1)
         if (items[i].name == BACKSTAGE) {
           if (items[i].sellIn < 11) {
             if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+              items[i].quality = modifyQuality(1)
             }
           }
           if (items[i].sellIn < 6) {
             if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+              items[i].quality = modifyQuality(1)
             }
           }
         }
@@ -43,15 +57,15 @@ export function updateQuality() {
         if (items[i].name != BACKSTAGE) {
           if (items[i].quality > 0) {
             if (items[i].name != SULFURAS) {
-              items[i].quality = items[i].quality - 1
+              items[i].quality = modifyQuality(-1)
             }
           }
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          items[i].quality = modifyQuality(0)
         }
       } else {
         if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+          items[i].quality = modifyQuality(1)
         }
       }
     }
